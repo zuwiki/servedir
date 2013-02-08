4c5611f33aa5ee6ebd124e5832c5d1d07df3837b
Michael Victor Zink <mvz@mvz.so>
2013-02-08 17:59:21 -0600

    Adding servedir.go

---
+++ servedir.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	endpt := flag.String("endpt", ":8080", "endpoint to listen on")
+	dir := flag.String("dir", "./", "directory to serve--be careful!")
+	flag.Parse()
+	http.Handle("/", http.FileServer(http.Dir(*dir)))
+	log.Print("Listening on", *endpt)
+	err := http.ListenAndServe(*endpt, nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
